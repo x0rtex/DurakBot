a88faa2bd32e9ed8d762c28c351d1d0fce498a2a
Alekss Belavskis <x0rtex420@gmail.com>
2024-11-24 19:21:47 +0000

    basic bot

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import discord
 import os
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
-description = "Testing"
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+GUILD = os.getenv("GUILD")
+
+MY_GUILD = discord.Object(id=GUILD)
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
 
 intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+client = MyClient(intents=intents)
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-
-@bot.event
+@client.event
 async def on_ready():
     """Called when bot is logged in."""
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
     print("------")
 
-@bot.command()
-async def add(ctx: commands.Context, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(str(left + right))
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
-load_dotenv()
-token = os.getenv("TOKEN")
-bot.run(token)
+@client.tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+
+client.run(TOKEN)
